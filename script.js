1144782f6a023d51664bd93333198227c801eef7
BreiSpicer <92058350+BreiSpicer@users.noreply.github.com>
2021-10-06 19:51:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principle = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principle * years * rate /100;
+    var year = new date().getFullYear()+parseInt(years);
 }
-        
+Function updaterate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementbyId("rate_val").innertext=rateval
+}
